f2b4d0084c7d2287df4a34f49fe7f46bac3282f9
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-03-13 15:58:55 +0200

    stuff

---
+++ basics/TS/src/image/stability.ts
@@ -0,0 +1,36 @@
+// takes a lot of experimentation to get the right image
+
+import { BedrockRuntimeClient, InvokeModelCommand } from '@aws-sdk/client-bedrock-runtime'
+import { writeFileSync, createReadStream } from 'fs'
+
+const client = new BedrockRuntimeClient({ region: 'us-west-2' })
+
+const stabilityImageConfig = {
+    text_prompts: [
+        {
+            text: 'a photo of a dragon',
+        }
+    ],
+    height: 512,
+    width: 512,
+    cfg_scale: 10,
+    style_preset: '3d-model',
+}
+
+async function invokeModel() {
+    const response = await client.send(new InvokeModelCommand({
+        modelId: 'stability.stable-diffusion-xl-v1',
+        body: JSON.stringify(stabilityImageConfig),
+        accept: 'application/json',
+        contentType: 'application/json'
+    }));
+    const responseBody = JSON.parse(new TextDecoder().decode(response.body));
+    saveImage(responseBody.artifacts[0].base64, 'dragonDiffusion.png');
+}
+
+function saveImage(base64Data: string, fileName: string) {
+    const imageBuffer = Buffer.from(base64Data, 'base64');
+    writeFileSync(fileName, imageBuffer);
+}
+
+invokeModel();
