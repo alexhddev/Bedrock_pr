9af57af0ec9f61633265b4df9c60293fddc6db1b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-04-02 11:20:56 +0300

    summary lambda

---
+++ text_api/ts/services/summary.ts
@@ -0,0 +1,56 @@
+import { BedrockRuntimeClient, InvokeModelCommand } from "@aws-sdk/client-bedrock-runtime";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+const AWS_REGION_BEDROCK = "eu-central-1";
+
+const client = new BedrockRuntimeClient({ region: AWS_REGION_BEDROCK });
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+
+    let response: APIGatewayProxyResult = {} as any;
+
+
+    if (event.body) {
+        const parsedBody = JSON.parse(event.body);
+        const numberOfPoints = event.queryStringParameters?.points
+        if (parsedBody.text && numberOfPoints) {
+            const text = parsedBody.text;
+            const titanConfig = getTitanConfig(text, numberOfPoints);
+
+            const response = await client.send(new InvokeModelCommand({
+                modelId: 'amazon.titan-text-express-v1',
+                body: JSON.stringify(titanConfig),
+                accept: 'application/json',
+                contentType: 'application/json'
+            }));
+            const responseBody = JSON.parse(new TextDecoder().decode(response.body));
+            const firstResult = responseBody.results[0];
+            if(firstResult && firstResult.outputText) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ summary: firstResult.outputText })                
+                }
+            }
+        }
+    }
+    response.statusCode = 400;
+    response.body = JSON.stringify({ message: "Invalid request" });
+
+    return response;
+}
+
+function getTitanConfig(text: string, points: string) {
+
+    const input = `Summarize in ${points} points the following text:
+        ${text}`;
+
+    return {
+        inputText: input,
+        textGenerationConfig: {
+            maxTokenCount: 4096,
+            stopSequences: [],
+            temperature: 0,
+            topP: 1,
+        },
+    };
+}
